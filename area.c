ab2fde6bef09e3cf0b36190a3b14015e9160485c
alanjose006 <alanjose1606@gmail.com>
2025-04-03 12:01:04 +0530

    second  push

---
+++ area.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+float radious,area;
+printf("enter the radious\n");
+scanf("%f",&radious);
+area = 3.14*radious*radious;
+printf("area of the circle=%f",area);
+}
